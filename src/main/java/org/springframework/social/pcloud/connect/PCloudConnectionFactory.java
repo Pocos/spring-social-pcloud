3058edbf1a929404a1310ec02295bcb2572eebb6
Pocos <pocosandrea@gmail.com>
2017-02-27 17:47:29 +0100

    Disabled state parameter support. Pcloud does not support it

--- src/main/java/org/springframework/social/pcloud/connect/PCloudConnectionFactory.java
+++ src/main/java/org/springframework/social/pcloud/connect/PCloudConnectionFactory.java
@@ -8,4 +8,12 @@ public class PCloudConnectionFactory extends OAuth2ConnectionFactory<PCloud> {
 	public PCloudConnectionFactory(String clientId, String clientSecret) {
 		super("pcloud", new PCloudServiceProvider(clientId, clientSecret), new PCloudAdapter());
 	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.social.connect.support.OAuth2ConnectionFactory#supportsStateParameter()
+	 */
+	@Override
+	public boolean supportsStateParameter() {
+		return false;
+	}
 }
